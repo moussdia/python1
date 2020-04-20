76395df39be4d35a2c2658b6141e1a5d19c86bac
Msc PRO <60006724+moussdia@users.noreply.github.com>
2020-04-20 12:26:35 +0000

    Create Akwaba_Fonction.py

---
+++ Akwaba_Fonction.py
@@ -0,0 +1,47 @@
+# 
+#TP COMPRENDRE LES FONCTIONS
+
+#Affiche le contenu de print 
+print ("Akwaba Python Calculator")
+'''
+#Declare l'argument de ma fonction et renvoyer les valeurs 'x' et 'y'
+def somme (x,y):
+    return x + y
+
+#Definir la fonction 'main' qui va faire la somme et afficher la somme de x et y 
+def main():
+    print("Faire la somme x et y")
+    x = int(input("Premier chiffre est:"))
+    y = int(input("Deuxieme chiffre est:"))
+    resultat = somme(x,y)
+    print("La somme de ", x ,"et", y ,"est",
+resultat)
+
+
+if __name__=="__main__":
+#Appel la fonction 'main()'	
+    main()
+'''
+
+#Fonction avec soustraction 
+
+
+#Declare l'argument de ma fonction et renvoyer les valeurs 'x' et 'y'
+def soustraction (x,y):
+    return x - y
+
+#Definir la fonction 'main' qui va faire la soustraction et afficher la soustraction de x et y 
+def main():
+    print("Faire la soustraction x et y")
+    x = int(input("Premier chiffre est:"))
+    y = int(input("Deuxieme chiffre est:"))
+    resultat = soustraction(x,y)
+    print("La soustraction de ", x ,"et", y ,"est",
+resultat)
+
+
+if __name__=="__main__":
+#Appel la fonction 'main()'	
+    main()
+    
+    
